f6f66c1862ecb3e51fb1f64cf1b572281926debe
anshika <anshikakesri02@gmail.com>
2024-10-29 12:34:45 +0530

    create signup

--- webnex/src/App.js
+++ webnex/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import login from './Components/login'
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <login />
     </div>
   );
 }
---
+++ webnex/src/Components/login/login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      <form action=" " method="get">
+        <label for="Email-id">Email Id</label>
+        <input type="email" placeholder='Email' id="Email-id"></input>
+      </form>
+    </div>
+  )
+}
+
+export default login
